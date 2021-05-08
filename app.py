75c058e292aae98cd40a5f4eedaec39d1a6a7525
irasan <iryna.sanzhara@gmail.com>
2021-05-08 20:34:34 +0000

    Add template for adding events

--- app.py
+++ app.py
@@ -101,19 +101,20 @@ def add_event():
     if "user" in session:
         if request.method == "POST":
             event = {
-                "name": request.form.get("name").lower(),
+                "name": request.form.get("event_name").lower(),
                 "description": request.form.get("description"),
                 "meetup_type": request.form.get("type"),
-                "city": request.form.getlist("city"),
-                "address": request.form.getlist("address"),
-                "date": request.form.getlist("date"),
-                "time": request.form.getlist("time"),
-                "topic": request.form.getlist("topic"),
+                "city": request.form.get("city"),
+                "address": request.form.get("address"),
+                "date": request.form.get("date"),
+                "time": request.form.get("time"),
+                "topic": request.form.get("topic"),
                 "added_by": session["user"]
                 }
-            mongo.db.meetuos.insert_one(event)
+            mongo.db.meetups.insert_one(event)
 
-            flash("Your Event Was Successfully Created and Added to the Calendar!")
+            flash(
+                "Your Event Was Successfully Created and Added to the Calendar!")
             return render_template("home.html")
 
         types = list(mongo.db.meetup_types.find().sort("meetup_type", 1))
