52926460c6bf5b1defe19a8b98d5234b7c98e321
emmasarge <emmasargeant206@gmail.com>
2021-05-10 10:45:38 +0000

    added iryna's code for script.js and style.css

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,6 @@
 $('.collapse').collapse()
 
+$(document).ready(function() {
+  $('li.active').removeClass('active');
+  $('a[href="’' + location.pathname + '"]').closest('li').addClass('active');
+});
